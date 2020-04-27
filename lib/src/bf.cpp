dc771bfc0040c1a92b27e77efa93e7b75ccdd165
wtlgo <georgeendo0@gmail.com>
2020-04-27 17:58:49 +0300

    Embeded parser into interpreter

--- lib/src/bf.cpp
+++ lib/src/bf.cpp
@@ -1,8 +1,11 @@
 #include <memory>
 
 #include <bf.hpp>
+#include <tokenizer.hpp>
+#include <parser.hpp>
 #include <AST.hpp>
 
 wtlgo::bf::Interpreter::Interpreter(const std::string& input, uint64_t opt) {
-
+    auto tokens = Tokenizer::tokenize(input.cbegin(), input.cend(), opt);
+    this->ast = Parser::parse(tokens.cbegin(), tokens.cend());
 }
