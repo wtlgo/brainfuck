7dcbb9537f732848c0c67f1272929bcd48f51539
wtlgo <georgeendo0@gmail.com>
2020-04-27 20:48:49 +0300

    Added VM

--- lib/include/private/VM.hpp
+++ lib/include/private/VM.hpp
@@ -1,8 +1,17 @@
 #ifndef _VM_
 #define _VM_
 
-struct VM {
+#include <iostream>
+#include <memory>
 
+#include <tape.hpp>
+
+struct VM {
+    std::unique_ptr<BasicTape> tape;
+    std::ostream& out;
+    std::istream& in;
+
+    VM(int64_t opt = 0, std::ostream& out = std::cout, std::istream& in = std::cin);
 };
 
 #endif
--- lib/src/VM.cpp
+++ lib/src/VM.cpp
@@ -1,1 +1,4 @@
 #include <VM.hpp>
+
+VM::VM(int64_t opt, std::ostream& out, std::istream& in) :
+    tape { TapeGenerator::generate(opt) }, out { out }, in { in } {}
